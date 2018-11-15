9aa3bdcb8f35db826bb38943546c4380625f959d
alvondirlj <a.junner@gmail.com>
2018-11-15 20:29:39 -0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    comments: [
+      'Comment 1',
+      'Comment 2',
+      'Comment 3',
+      'Comment 4'
+    ]
+  }
+  sendComment = () => {
+    this.setState({
+      comments: [...this.state.comments, 'Comentário']
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        { /* NewComment */}
+        <div>
+          <textarea></textarea>
+          <button onClick={this.sendComment}>Enviar</button>
+        </div>
+        { /* Comments */}
+        <div>
+          { /* Comment */}
+          { this.state.comments.map( c => {
+            return <div>{c}</div>
+          })}
+        </div>
       </div>
+
     );
   }
 }
